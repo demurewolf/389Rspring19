768b829f95d50b5d26e46978077f7f23811c11b1
demurewolf <josiahwedgwood@ymail.com>
2019-04-22 20:19:57 +0000

    Part one of assingment completed

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,16 +4,26 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hash_file = open("hashes.txt") # open and read hashes.txt
+    passwords = open("passwords.txt") # open and read passwords.txt
     characters = string.ascii_lowercase
+    
+    hashes = hash_file.read().split('\n')
 
-    for c in characters:
-        for p in passwords:
+    for p in passwords:
+        for c in characters:
             # crack hashes
 
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+            context = hashlib.sha256()
+
+            test = (c + p).rstrip()
+            context.update(bytes(test, 'ascii'))
+            d = context.hexdigest()
+            
+            if d in hashes:
+                print("{}:{}".format(test, d))
 
 if __name__ == "__main__":
     crack()
