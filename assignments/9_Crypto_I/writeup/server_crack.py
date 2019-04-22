af93cd8a202df0f26276433d395f28b521be0d46
demurewolf <josiahwedgwood@ymail.com>
2019-04-22 22:05:24 +0000

    Finished part 2 and found server flag

--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,17 +6,53 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    flag = ""
+
+    password_file = open("passwords.txt", 'r')
+    passwords = password_file.read().split('\n')
+
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
     data = s.recv(1024)
     # parse data
     # crack 3 times
+    crack = 0
+    
+    while data != b"":
+        if crack == 3:
+            flag = data.decode("ascii").rstrip()
+            break
+
+        serv_h = data.decode("ascii").split("\n")[2].rstrip()
+        found = False
+        print("Finding input for {}".format(serv_h))
+        
+        for p in passwords:
+            if not found:
+                for c in characters:
+                    context = hashlib.sha256()
+
+                    test = (c + p).rstrip()
+                    context.update(bytes(test, 'ascii'))
+                    h = context.hexdigest()
+                    
+                    if serv_h == h:
+                        found = True
+                        s.send(bytes(test + '\n', 'ascii'))
+                        print("Sent {}".format(test))
+                        crack += 1
+                        
+        if found:
+            data = s.recv(1024)
+        else:
+            print("Couldn't find hash for {}".format(serv_h))
+            data = b""
+
+    print(flag)
 
 if __name__ == "__main__":
     server_crack()
